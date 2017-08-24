247d6e5137a216db57e00e424be1ff7671018074
zeroincombenze <info@shs-av.com>
2017-08-24 22:34:47 +0200

    [SYNCHRO] Synchronizing against OCA repository

---
+++ account_banking_payment/model/__init__.py
@@ -0,0 +1,7 @@
+from . import account_payment
+from . import payment_line
+from . import payment_mode
+from . import payment_order_create
+from . import banking_import_transaction
+from . import banking_transaction_wizard
+from . import banking_import_line
